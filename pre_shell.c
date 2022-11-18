1bf0355a179c7dd19441a9e4927e689268c178b1
agbuenoch <agbuenoch38@gmail.com>
2022-11-18 07:12:18 +0000

    Simple shell commit

--- pre_shell.c
+++ pre_shell.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 /**
- * main - program that prints all the arguments, without using ac.
+ * This is a super simple shell that can run command
+ * with their full path, without any arguement.
  *
  * Return: zero
 */
 int main(__attribute__((unused))int argc, char *argv[])
 {
-
+  
   printf("#cisfun$ ");
   scanf("%s", *argv);
+
 
   if (execve(argv[0], argv, NULL) == -1)
     {
       perror("./shell");
+      return (1);
     }
 
   
-return (0);
+  return (0);
 }
